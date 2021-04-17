c594d6c4bdef00ea9aea7108d53fb050cfd03ae4
Dhayana N. Silva <contatodhayana@gmail.com>
2021-04-17 00:41:45 -0300

    Cria estilos globais

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import GlobalStyles from "./GlobalStyles";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  background-color: #f2f2f2;
+`;
+
 function App() {
   return (
-    <div>
-      <h1>Parking</h1>
-    </div>
+    <Container>
+      <GlobalStyles />
+    </Container>
   );
 }
 
