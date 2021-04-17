6d561a03dce5f6558a67aa1b180fdd9491111759
Dhayana N. Silva <contatodhayana@gmail.com>
2021-04-17 01:23:52 -0300

    Adiciona fonte

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -1,18 +1,18 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
-    * {
-        margin: 0;
-        padding: 0;
-        outline: 0;
-        box-sizing: border-box;
-        font-family: sans-serif;
-    }
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+    font-family: "Open Sans", sans-serif;
+  }
     
-    body {
-        width: 100%;
-        height: 100vh;
-    }
+  body {
+    width: 100%;
+    height: 100vh;
+  }
 `;
 
 export default GlobalStyles;
